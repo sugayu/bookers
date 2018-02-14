fddfa7756eb845d581794580eaaf96731c4cdf42
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-02-14 16:05:15 +0900

    create controller

---
+++ bookers/app/controllers/books_controller.rb
@@ -0,0 +1,13 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def books
+  end
+end
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'books/index'
+
+  get 'books/show'
+
+  get 'books/edit'
+
+  get 'books/books'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
